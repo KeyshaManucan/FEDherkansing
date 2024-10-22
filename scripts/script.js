333fa4de56c8349e3c9eca3502dd33f33f0a453e
KeyshaManucan <keyshajoyce20@gmail.com>
2024-10-22 19:29:53 +0200

    discover menu start

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,20 @@ function sluitMenu () {
     let deNav = document.querySelector("nav");
     deNav.classList.remove("toonMenu");
 }
+
+// discover menu
+let discoverMenu = document.querySelector("main nav button:nth-of-type(2)")
+let discoverList = document.querySelector("main nav ul")
+
+discoverMenu.addEventListener('click', menuToggle)
+
+function menuToggle () {
+    discoverList.classList.toggle('discoverMenu') 
+}
+
+// arrow rotatie discover menu
+let arrowRotatie = document.querySelector("button[aria-label='arrow']");
+arrowRotatie.onclick = startRotate;
+function startRotate() {
+  arrowRotatie.classList.toggle("rotate");
+}
